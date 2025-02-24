04774debe062f627195b70ab3c8a558e55cd932f
Dominique Harris <revnique@gmail.com>
2025-02-24 11:12:59 -0600

    started react project from scratch using vite, sass and react router

---
+++ react/react-portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import { BrowserRouter } from 'react-router'
+import { HomePage } from './HomePage/HomePage'
+function App() {
+  return (
+    <BrowserRouter>
+    <HomePage />
+    </BrowserRouter>
+  )
+}
+
+export default App
