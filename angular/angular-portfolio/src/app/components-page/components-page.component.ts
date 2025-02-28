1c9deabe64986436d5cb100e8553e32d3acce7d7
Dominique Harris <revnique@gmail.com>
2025-02-28 07:40:51 -0600

    slider to component page NG

--- angular/angular-portfolio/src/app/components-page/components-page.component.ts
+++ angular/angular-portfolio/src/app/components-page/components-page.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './components-page.component.scss'
 })
 export class ComponentsPageComponent {
+  valueBarPositiveHeight = 70;
+  valueBarNegativeHeight = 30;
 
+  updateValueBarPositiveHeight(value: number) {
+    console.log(value);
+    this.valueBarPositiveHeight = value;
+    this.valueBarNegativeHeight = 100 - value;
+    console.log(this.valueBarPositiveHeight, this.valueBarNegativeHeight);
+  }
 }
